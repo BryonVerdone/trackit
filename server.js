bdd3038f73a7dab6377d5540b00c8e3d7ed56cdc
bryonverdone <bmverdone@gmail.com>
2022-11-18 11:00:08 -0500

    added routes, controllers and views. Able to render html.

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
-
+const mainRoutes = require('./routes/main');
 require('dotenv').config({ path: './config/.env' });
 require('dotenv').config({ path: './config/.env' });
+app.use('/', mainRoutes);
 app.listen(process.env.PORT, () => {
   console.log(`server is running on port `, process.env.PORT);
   // console.log(secret);
