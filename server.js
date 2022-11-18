bb130ee282591e877f0d74a87305781ad7e0c778
bryonverdone <bryonverdone@gmail.com>
2022-11-17 21:08:42 -0500

    node server running

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-const server = 'node';
+const express = require('express');
+const app = express();
+
+require('dotenv').config({ path: './config/.env' });
+require('dotenv').config({ path: './config/.env' });
+app.listen(process.env.PORT, () => {
+  console.log(`server is running on port `, process.env.PORT);
+  // console.log(secret);
+});
