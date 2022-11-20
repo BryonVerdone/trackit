c83ed29554bb4b30bfde98145afafbee99fe64d7
bryonverdone <bryonverdone@gmail.com>
2022-11-19 20:34:24 -0500

    added connection string DB connected

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const connectDB = require('./config/database');
 const mainRoutes = require('./routes/main');
+const mongoose = require('mongoose');
 require('dotenv').config({ path: './config/.env' });
-require('dotenv').config({ path: './config/.env' });
+connectDB();
 app.use('/', mainRoutes);
 app.listen(process.env.PORT, () => {
   console.log(`server is running on port `, process.env.PORT);
