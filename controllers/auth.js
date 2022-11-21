2116fb8fa2f89887ca6b6f89d9d579cd1cf177d2
bryonverdone <bryonverdone@gmail.com>
2022-11-20 23:04:14 -0500

    added routes to login and signup page

---
+++ controllers/auth.js
@@ -0,0 +1,19 @@
+const passport = require('passport');
+const validator = require('validator');
+const User = require('../models/User');
+
+exports.getLogin = (req, res) => {
+  if (req.user) {
+    return res.redirect('/profile');
+  }
+  res.render('login');
+};
+
+exports.getSignup = (req, res) => {
+  if (req.user) {
+    return res.redirect('/profile');
+  }
+  res.render('signup', {
+    title: 'Create Account',
+  });
+};
--- routes/main.js
+++ routes/main.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/home');
+const authController = require('../controllers/auth');
 
+//main routes
 router.get('/', homeController.getIndex);
-
+router.get('/signup', authController.getSignup);
+router.get('/login', authController.getLogin);
 module.exports = router;
