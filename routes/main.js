12527f9e11ff442e9a53b10476cb87390f4ca16f
bryonverdone <bryonverdone@gmail.com>
2022-11-24 19:34:40 -0500

    able to create user, still need to render profile after creation

--- routes/main.js
+++ routes/main.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/home');
 const authController = require('../controllers/auth');
-
+const { ensureAuth } = require('../middleware/auth');
 //main routes
 router.get('/', homeController.getIndex);
+router.get('/profile', ensureAuth, postsController.getProfile);
 router.get('/signup', authController.getSignup);
 router.post('/signup', authController.postSignup);
 router.get('/login', authController.getLogin);
--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const mainRoutes = require('./routes/main');
 const mongoose = require('mongoose');
 require('dotenv').config({ path: './config/.env' });
 connectDB();
+//static files
 app.use(express.static('public'));
+//body parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.set('view engine', 'ejs');
 app.use('/', mainRoutes);
 app.listen(process.env.PORT, () => {
